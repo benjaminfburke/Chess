f004d39b25553e24d5413b45fdc8240d9b101b96
Joseph Schmidt <schmi150@purdue.edu>
2021-12-05 13:13:06 -0500

    Board continuing

--- frontend/src/component/Board.js
+++ frontend/src/component/Board.js
@@ -1,68 +1,75 @@
 import React from "react";
 import { Chessboard } from "react-chessboard";
 const jsonWeb = require("jsonwebtoken");
-const chessBoard = require("Chessboard");
+const Chess = require("chess.js");
+let Game = new Chess();
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.onDrop = this.onDrop.bind(this);
     this.state = {
-      a1:"wR",
-      b1:"wN",
-      c1:"wB",
-      d1:"wQ",
-      e1:"wK",
-      f1:"wB",
-      g1:"wN",
-      h1:"wR",
-      a2:"wP",
-      b2:"wP",
-      c2:"wP",
-      d2:"wP",
-      e2:"wP",
-      f2:"wP",
-      g2:"wP",
-      h2:"wP",
-      a7:"bP",
-      b7:"bP",
-      c7:"bP",
-      d7:"bP",
-      e7:"bP",
-      f7:"bP",
-      g7:"bP",
-      h7:"bP",
-      a8:"bR",
-      b8:"bN",
-      c8:"bB",
-      d8:"bK",
-      e8:"bQ",
-      f8:"bB",
-      g8:"bN",
-      h8:"bR"
+      //   a1: "wR",
+      //   b1: "wN",
+      //   c1: "wB",
+      //   d1: "wQ",
+      //   e1: "wK",
+      //   f1: "wB",
+      //   g1: "wN",
+      //   h1: "wR",
+      //   a2: "wP",
+      //   b2: "wP",
+      //   c2: "wP",
+      //   d2: "wP",
+      //   e2: "wP",
+      //   f2: "wP",
+      //   g2: "wP",
+      //   h2: "wP",
+      //   a7: "bP",
+      //   b7: "bP",
+      //   c7: "bP",
+      //   d7: "bP",
+      //   e7: "bP",
+      //   f7: "bP",
+      //   g7: "bP",
+      //   h7: "bP",
+      //   a8: "bR",
+      //   b8: "bN",
+      //   c8: "bB",
+      //   d8: "bK",
+      //   e8: "bQ",
+      //   f8: "bB",
+      //   g8: "bN",
+      //   h8: "bR",
+      fen: "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
     };
   }
 
   onDrop(sourceSquare, targetSquare) {
+    Game.move({ from: sourceSquare, to: targetSquare });
     console.log(sourceSquare);
     console.log(targetSquare);
     this.setState({
-      [sourceSquare]: "",
-      [targetSquare]: this.state[sourceSquare],
+      fen: Game.fen(),
     });
-    var string = chessBoard.objToFen(this.state);
-    console.log(string);
+    if (Game.game_over()) {
+      console.log("Winner");
+    }
   }
   render() {
     return (
       <div>
         <Chessboard
           id="BasicBoard"
-          position={this.state}
+          position={this.state.fen}
           onPieceDrop={this.onDrop}
         />
       </div>
     );
+  }
+  componentDidMount() {
+    Game = new Chess(this.state.fen);
+    console.log(Game.fen());
   }
 }
 
